0c6ea6b6ced5ac618a3c682b1eaa7ccc345a732f
val <valmobil@gmail.com>
2019-01-07 18:38:37 +0200

    hashMap - findTripleIndex v4

--- src/hashMapFindTripleIndex/Main.java
+++ src/hashMapFindTripleIndex/Main.java
@@ -1,9 +1,10 @@
 package hashMapFindTripleIndex;
 
-import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static readInputFromFile.ReadFromFile.readFromFile;
 
 public class Main {
 
@@ -11,88 +12,91 @@ public class Main {
     public static void main(String[] args) {
 
 
-//        System.out.println(factorial(100000L));
-
-
+        //Manual input
 //        String test = "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1";
 //        String test = "1 2 2 2 3 4 8";
 //        String test = "1 1 1 1 1 2 2 2 2 2";
-//        String[] arr = test.split(" ");
-        long[] arrInt = new long[100000];
-        Arrays.fill(arrInt, 1);
-//        List<Long> longList = Stream.of(arr).map(Long::valueOf).collect(Collectors.toList());
-        List<Long> longList = Arrays.stream(arrInt).boxed().collect(Collectors.toList());
+//        String[] arrString = test.split(" ");
+//        long[] arrInt = new long[100000];
+//        Arrays.fill(arrInt, 1);
+//        List<Long> arr = Stream.of(arrString).map(Long::valueOf).collect(Collectors.toList());
+//        List<Long> longList = Arrays.stream(arrInt).boxed().collect(Collectors.toList());
 //        List<Long> longList = Arrays.asList(arrInt);
-        System.out.println(countTriplets(longList, 1L));
-        System.out.println(countTripletsOld(longList, 1L));
 
+        //Input From File
+        List<Long> arr = readFromFile("src/hashMapFindTripleIndex/input/input4.txt");
+
+        System.out.println(countTriplets(arr, 3L));
+//        System.out.println(countTripletsOld(arr, 1L));
     }
 
 
     // Complete the countTriplets function below.
     static long countTriplets(List<Long> arr, long r) {
-        int numberOfLevels = 3;
+//        int numberOfLevels = 3;
         long count = 0;
-        Map<Long, Long> arrLevel1 = new LinkedHashMap<>();
-        Map<Long, Long> arrLevel2 = new LinkedHashMap<>();
-        Map<Long, Long> arrLevel3 = new LinkedHashMap<>();
+        Map<Long, long[]> arrLevel1 = new LinkedHashMap<>();
 //        Long curMinValueLevel = Long.MIN_VALUE;
+        long prevValue = 0L;
+        long repetitiveCount = 0L;
         for (Long element : arr) {
-            //Level 1
-            if (arrLevel1.containsKey(element)) {
-                arrLevel1.put(element, arrLevel1.get(element) + 1);
+            if (element.equals(prevValue)) {
+                repetitiveCount++;
+                continue;
             } else {
-                arrLevel1.put(element, 1L);
-            }
-            //Level 2
-            for (Map.Entry<Long, Long> entryLevel1 : arrLevel1.entrySet()) {
-                if (entryLevel1.getKey() * r == element) {
-                    if (arrLevel2.containsKey(element)) {
-                        arrLevel2.put(element, arrLevel2.get(element) + 1);
-                    } else {
-                        arrLevel2.put(element, 1L);
-                    }
+                if (repetitiveCount > 0) {
+                    addElement(prevValue, repetitiveCount, r, arrLevel1);
+                    repetitiveCount = 0L;
                 }
+                addElement(element, 1L, r, arrLevel1);
+                prevValue = element;
             }
-            //Level 3
-            for (Map.Entry<Long, Long> entryLevel2 : arrLevel2.entrySet()) {
-                if (entryLevel2.getKey() * r == element) {
-                    if (arrLevel3.containsKey(element)) {
-                        arrLevel3.put(element, arrLevel3.get(element) + 1);
-                    } else {
-                        arrLevel3.put(element, 1L);
-                    }
-                }
-            }
+//            if (arrLevel1.size() > 100) {
+//                System.out.println();
+//            }
+        }
+        if (repetitiveCount > 0) {
+            addElement(prevValue, repetitiveCount, r, arrLevel1);
         }
         //Count Calculation
 
-        for (Map.Entry<Long, Long> entryLevel1 : arrLevel1.entrySet()) {
-            Long entryValueLevel2 = arrLevel2.get(entryLevel1.getKey() * r);
-            if (entryValueLevel2 == null) entryValueLevel2 = 0L;
-            Long entryValueLevel3 = arrLevel3.get(entryLevel1.getKey() * r * r);
-            if (entryValueLevel3 == null) entryValueLevel3 = 0L;
-            if (entryLevel1.getValue().equals(entryValueLevel2) && entryValueLevel2.equals(entryValueLevel3)) {
-                count = (factorial(entryLevel1.getValue()).divide(factorial(entryLevel1.getValue() - numberOfLevels).multiply(factorial((long) numberOfLevels)))).longValue();
+        for (Map.Entry<Long, long[]> entryLevel1 : arrLevel1.entrySet()) {
+            Long entryValueLevel1 = entryLevel1.getValue()[0];
+            Long entryValueLevel2 = entryLevel1.getValue()[1];
+            Long entryValueLevel3 = entryLevel1.getValue()[2];
+            if (entryValueLevel1.equals(entryValueLevel2) && entryValueLevel2.equals(entryValueLevel3)) {
+                count += (entryValueLevel1 * (entryValueLevel1 - 1) * (entryValueLevel1 - 2)) / 6;
             } else {
-                count += entryValueLevel3 * entryValueLevel2 * entryLevel1.getValue();
+                count += entryValueLevel3 * entryValueLevel2 * entryValueLevel1;
             }
         }
         return count;
     }
 
-
-    public static BigInteger factorial(Long number) {
-        String value;
-
-        BigInteger result = new BigInteger("" + 1);
-
-        for (long factor = 2; factor <= number; factor++) {
-            result = result.multiply(new BigInteger("" + factor));
+    public static void addElement(Long element, Long repetitiveCount, Long r, Map<Long, long[]> arrLevel1) {
+        //Possible improvement:
+        //remove 2 maps
+        //Level 1
+        if (arrLevel1.containsKey(element)) {
+            long[] tempArr = arrLevel1.get(element);
+            tempArr[0]+=repetitiveCount;
+        } else {
+            arrLevel1.put(element, new long[]{1L, 0L, 0L});
         }
-
-        return result;
+        //Level 2
+        Long elementMinus1 = element / r;
+        if (arrLevel1.containsKey(elementMinus1) && elementMinus1*r == element) {
+            long[] tempArr = arrLevel1.get(elementMinus1);
+            tempArr[1]+=repetitiveCount;
+        }
+        //Level 3
+        Long elementMinus2 = elementMinus1 / r;
+        if (arrLevel1.containsKey(elementMinus2) && elementMinus2*r == elementMinus1) {
+            long[] tempArr = arrLevel1.get(elementMinus2);
+            tempArr[2]+=repetitiveCount;
+        }
     }
+
 
     static long countTripletsOld(List<Long> arr, long r) {
         List<Long> arrList = new ArrayList(arr);
