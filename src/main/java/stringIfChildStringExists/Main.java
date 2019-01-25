f7641314423c9adcecb75d7997a0e39d3db2e4b7
val <valmobil@gmail.com>
2019-01-25 17:53:20 +0200

    string - if String is Substring - final Dynamic Programming

--- src/main/java/stringIfChildStringExists/Main.java
+++ src/main/java/stringIfChildStringExists/Main.java
@@ -2,6 +2,7 @@ package stringIfChildStringExists;
 
 import com.sun.corba.se.spi.ior.TaggedProfileTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 
@@ -17,43 +18,18 @@ public class Main {
 
     // Complete the commonChild function below.
     static int commonChild(String s1, String s2) {
-        int maxString = 0;
+        int length = s1.length();
+        int[][] arr = new int[length + 1][length + 1];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    arr[i + 1][j + 1] = arr[i][j] + 1; }
+                else {
+                    arr[i + 1][j + 1] = Math.max(arr[i][j + 1], arr[i + 1][j]);
 
-        //calculate max binary
-        int maxBinary = (int) Math.pow(2, s1.length()) - 1;
-
-        //Create dictionary from first string
-        HashSet<String>[] hashSet = new HashSet[s1.length() + 1];
-        for (int i = 0; i < s1.length() + 1; i++) {
-            hashSet[i] = new HashSet();
-        }
-        for (int i = 0; i < maxBinary; i++) {
-            String str = Integer.toBinaryString(i + maxBinary + 1);
-            StringBuilder sb = new StringBuilder(s1);
-            for (int j = s1.length() - 1; j >= 0; j--) {
-                if (str.charAt(j + 1) == '1') {
-                    sb.delete(j, j + 1);
-                }
-            }
-            hashSet[sb.length()].add(sb.toString());
-        }
-        //Parse second string and check if exists in hashSet
-        for (int i = 0; i < maxBinary; i++) {
-            String str = Integer.toBinaryString(i + maxBinary + 1);
-//            System.out.println(str);
-            StringBuilder sb = new StringBuilder(s2);
-            for (int j = s1.length() - 1; j >= 0; j--) {
-                if (str.charAt(j + 1) == '1') {
-                    sb.delete(j, j + 1);
-                }
-            }
-            if (sb.length() > maxString) {
-                if (hashSet[sb.length()].contains(sb.toString())) {
-                    maxString = Math.max(maxString, sb.length());
-                    System.out.println(sb);
                 }
             }
         }
-        return maxString;
+        return arr[length][length];
     }
 }
