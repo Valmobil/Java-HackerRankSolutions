43aba25b160df3592e85f420c76badab16eadf9d
val <valmobil@gmail.com>
2019-01-31 22:05:59 +0200

    hashMap - UnMergeShuffleWord final + tests

--- src/main/java/hashMapUnMergeShufleWord/Main.java
+++ src/main/java/hashMapUnMergeShufleWord/Main.java
@@ -1,16 +1,12 @@
 package hashMapUnMergeShufleWord;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-//        String input = "eggegg";
-//        String input = "aeiouuoiea";
-//        String input = "abcdefgabcdefg";
-//        String input = "bdabaceadaedaaaeaecdeadababdbeaeeacacaba"; //answer aaaaaabaaceededecbdb
-        String input ="ppzpzzzjzrzbzzpriirbbzizirbzrbpbzbbrzpjepzrzbbiibezbjeiebzebpjzerzpipbiezizzebeipzbrpbrzrbrzebipiziizrzbrebzrpezeerbzzejripebiibzpizrzpzzbrpbzezbzerjjjeipzirzzzeizbzjpzbijzeirepizpeizbzepjjzjiezbrbeziippzezbpezepzebzprziiiipbjepizzpipbzbziriibeieerrzzbeberebirrzzpiiebbpreijirzzzbepzbijizijzpepbepbiizezebbbbpbzrzizbbeibjpjiizbzeibizbzipzpzejizprbezbijbibepjbpbjezzirjizbbzpizzpbzbbrepbzzzzbreezipiizrrbzeepzppreeiijipbzrzjpiipeiizejzzzbibzbrjppjrzzzbbbbbbiizjeeeipjbrzibizzrejpzezjzzebzzjbbrjjrpeeiizjizzijzeziejeeeeireiebrrppzibbzizrzbiiizbjebeibzejpbizrziibbpiiezpjirjpjjizbbzbbbbizpbzibjirizzprirepziibbibiibzbezeezipjrbzeizjpepeibrzizeebbpjbibibizezzbrrbepzbippbipebbbrejeprierbzebrjbzrzzzbrebpbzrjiejzizbbrbbzbzzibbjzipbbpzzpbezerzzebbpibrijzrezizrezerbeirzbpbrbeiirpjzipziiirbziirizbzzbizzizjpjibzbijbrripziibbzjbibjzeizjjrbzjbibjzebzebijzpirbizizpbiibbjebiiiiebzbbzzrezezjizibiejzibjrbpribbbbbzzrpzjrrbbjibjprbieiezjbibpzbbzzibipzrebbizezibbbpizipbbrbzpieppzpiprrezeiprrbrbbibiebbbiiizzjzbezzrebeibzbzijbzzpzirirpiiiiezizbjbbzzbiizizjijzzzbbeizzzzzzbbebieijbzjizbbpzzizpeezbeeiribzizzeepzpbzziibjpeebijzizibzrzezbirirepzzzriipbibjirizzpjrzzjiizpejjjiberpzejijrjrzrzbbrjpzjbiebjzbbbzzjbbzepzrpizrrizzpzeeiieijbeiizzibbibjpbbippizijziirzrpeberrzezjrprpbrbbizipjzipjbbzbzpiirpjzbzbrzeizrzjzzjjebrzrizirzzieibpzbibeizbepbirbiibirjeibzizbzpzpzrrzzbpzzzpiziijizripbezipprrjbiejzzbjpepbjiirpipijiebibbbbijjpbiizpezrizibjzzrerjezepzeiijibriziizpizbzipberzbppjibipezbrbbrbijbeibzzzezizbipiiirpzippzejbiiijjjijrziribberzjzrebizijiiierzbjbrzrijrjrzrzzrbzjezzjjrzrjbrjiiiijepzijjbrzbbbbbpijjrjbzjjeiipipizbjijpzzbibjizipibeiizrbzpbbrreirjzzbiizzbezibjzjprzriijrprjbjezribiiriipibzppjeiiizbjbbjiezzjbibrizpjebjreerbrpjbrieejbbizjirizbrpijbjirbzjrezpzzbiijzrbzzbrerizpepzrjzzijzibejpjzppijpjrbpzzribbzzibrjzzrizzrzbbpirirpzzzzibbjibiijeiizipjbzejiijezzbijzbijbjbbpbezeijzbrebbepjbbbjprrrpebzrzizjziizrjiziiipiebpppjjipzziizbbrejzpzrrrbiziiiiiipjrbiebbijiibzizbbpzzizijjjirzzibejzijbzbjirpzpiriizppzpzeejizizbizzbbpjebzibbpbrbebzbzejiipezzeirieerrzirzbriirrrzjzibpziiribbejbjbepjbezipzzijbjzzrzrpieizbeizzzrizrripjzbjbzjziziiizzibziiijipziejbjjibjjzjzrzibpzzziipiijzjrrzzjbbrpbepipejezeibzezzjzezjzeeiepjbzjbzbjzbbieirbiriezzzbirirpizzzibbebjierjzipejbzerzizjpzbpbiiijiriizriiiepzpzirzbirerbrzebjibizizrbzjizbeeizibpbrrrbjrizizjbziebizijjpppibziezzrpirpbpbizpijpjeiizjibzebpbbrrziiizebbbibzpieipbepzbierbebzzbirbpbzbibiijbrbrzzbizjirzbzzbprizbjpzezpbzjzieiirbiiezezzjzpziijirjieeezbzzpzppjrzzpjzbibrrbbrbzzezbrpibrrbeeezirbzierrzebzjpjzbzbibipjbebejijziepzzizbizrzpziiiebebiireiebbzzzpeebbzizriiiibbbiibizprrjbibezzrbpzppirrpiireirrzbjperjzbjzeeejzzbeebiizibbeibbiribjibjjreepzrezijrbrzibzzjijipbbzzpbrzjzrjzzbziizripbjzzipebpzzebpjipbijrrrizbrppizbzbibbzbjpiepizrbpzzbpbprbzezpzirrbrzrbjzizijpzppjzirizrizbziberrziijebrrepribpjibbbpizibbijjzzipjirzrzepzirizbzzzijpzjierpiezbjjjpiiirppibzrbibzbppziiribbiejzebibjjzrzrjzzrzjibrzzibirjbpbpbiiiiiiizpbiiiebzjrziieezzzzizzirzzzibjbrzbirzzziprrjiibeizrjbiebzizzbizbpizziziipjibippiizzzpzrirjjiejjzejbipjibzbiiejiribjebrpirrrpiijpzizbeiezzrjbbbbjzbbbzibjrbbrrzzbbzrjrzzjbrjpzrjirjjrijrpieebrezipbebririiiiizeeiepbjbbbbbrrzbibbbibpizzppziiizeriipjizijrreprbijibrjejbzrzrzzziizbeirpreiizrbbirpjzjzzjrrzeibbbpzjijipizipzzbbebjzijbeziieizieieiebzrejzpzezizeizzpzeebzzziziibzepbbzzbiiriibrbzribejbjzbziiziezibibbzpizjzzizbzebieierpzebzijpizpbzzbbzzjpzizbrjziripbizzjijbjbrrzziiezzrirbjzzbiiibrizejiiiepjipbzbbbzzijjzziezejzberjzizzbzbbzepeibejjjpieibrpzpezrzbzzbprirrjzpjrbibjizppzbizzzrjrjerrrejibiezpzrbizbjzbbzjbjpirzrrizrpbpriiipbirbijprizzzziipbbbeijzpbzbebebbbbreizbebrrzbbjrzijiizpiijbribjbzzzzpzbzzbrbzezpiprjrpzbeezbzizezebjbbbizbjirpbziebbiibbeibzieezipjpbipzjbziiijpijijrzbzppiziiijrizibzzbzrzrjpzbibbjrzjbbiebzprzibbiebrzezpjzjbibrerbebzjzpbieijirzzbbrjezzrirzrirzzbjrpizbpebibebjezzbzjeieieizzbiezziziiripizizrrjiirbzjzbiiieebzpjpbiezzpbjpjzrpjbejpjzjbjieirizzbizpzerzzbiiiiibreijiibepezebrjbzrbbpbibzrijjbrpzzrjppiipjzpzippzeiipiizzzeibbzziebzzzipijijbrb";
-        System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbiibijrrriirbrirreeirieeiibbbrbbbibejbbzibzizijeezpzppziiripzprijbzbjizjbizzrijjjizizzbbziziijieirjiiiiiizirrrzpzjerziizzijjeiiiizijrziizjzizrzeprrrjjeerzjiezejjizjizzejiijezjpiziiejiiijizzzzpriripzrzzirzzjrizzirzzprjpjippzjpjeizjizzjrzpepzirerzzrzjiizzpzerjzrijjiprzirijzijeeirjprreerjejpzirijzzeijjziiiejppzipiiriiirzejjrprjiirzrpjzjizezziizzjrierrpzrziieipizijizzpjijzipipiiejjzjrjjipzrjjizpejiiiijrjrzrjjzzejzrzzrzrjrjirzrjzreiiijizierzjzreirizrjijjjiiijezppizpriiipizizezzziejirrzepiijppzrepizzipziizirijiiezpezejrerzzjizirzepziipjjiieijipipriijpepjzzjeijrrppizepirzijiizipzzzpzzrrzpzpzziziejriiiripezieizpieizzrizirzrejjzzjzrziezrzzjpriipzzjpizjpizirprprjzezrreeprzriizjizippipjiizziiejieiieezpzzirrziprzpezjzzzjeijzpjrzrzrjrjijezpreijjjepziijzzrjpzzirijipiirzzzperirizezrzizizjieepjiizzpzpeezzizirieezeepzizzpzijzjieiezzzzzziezzzjijziziizzjzizeiiiipririzpzzjizzieerzzezjzziiieiirrrpiezerrpipzppeipzrpizipizezierzpiizzzpijzeieirpjijrrjzprzzirprjizjeiizijzezerzzzeiiiiejiipziziripzjiezezjijzrjjziezjijziizpirrjizijpjzizzizzziriizriiizpizjpriierpzrierezerzizerzjiripezzrezepzzppizjizzzrzizjeijrzperzzzrzjrezreirpejerepippizperrzzeziiijpeezizriepepjziezrjpizeezeziiiiizperirpzzirijizpzizzijjpjrijpzeiipiizrzipjezieejziiizrzizizpprreierieeeejeizezjizzijziieeprjjrjzzezzjzezpjerzziizrjpieeejziizzzrjppjrzizzzjeziiepiipjzrzpijiieerppzpeezrrziipizeerzzzzperzpzzipzzijrizzejpjpeijizerpzijezpzpizziiezziijpjiezizrzpezeziipepepzjizijizpezzzrijierpeiipzzrriereezzrreeieiirizzpipzzipejpiiiizrpzezpezepzezppiizerzeijzjjpezziepziperiezjizpjzziezzzrizpiejjjrezzezprzzpzrzipziiepirjezzrzprzrzrziizipizrrzrprzpizzizipipzrzjpzijziizrzpjpzrzprzrizizriirpzzzrzjzzzpzpp");
+        String input = "aeiouuoiea";
+        //Expected result
+        System.out.println("eaid");
         System.out.println(reverseShuffleMerge(input));
     }
 
@@ -28,50 +24,63 @@ public class Main {
         }
         hashMap.replaceAll((k, v) -> v / 2);
 
+        HashMap<Character, Integer> hashMapCurrentQty = new HashMap<>(hashMap);
+        HashMap<Character, Integer> hashMapSkipped = new HashMap<>(hashMap);
+        int lastIndex;
         for (int i = s.length() - 1; i >= 0; i--) {
-            //remove letters already in result
-            HashMap<Character, Integer> hashMapCurrentQty = new HashMap<>(hashMap);
-            for (char ch : result.toString().toCharArray()) {
-                hashMapCurrentQty.replace(ch, hashMapCurrentQty.get(ch) - 1);
-            }
-            //calculate skipped letters
-            HashMap<Character, Integer> hashMapSkipped = new HashMap<>(hashMap);
-            for (int j = s.length() - 1; j > i; j--) {
-                hashMapSkipped.replace(s.charAt(j), hashMapSkipped.get(s.charAt(j)) - 1);
-            }
+            lastIndex = i;
+
             //calculate next letter
+            HashMap<Character, Integer> hashMapSkippedCurrent = new HashMap<>();
             char curChar = s.charAt(i);
-            while (hashMapCurrentQty.get(curChar) < 1 && i>0) {
-                hashMapSkipped.replace(curChar, hashMapSkipped.get(curChar) - 1);
+            while (hashMapCurrentQty.get(curChar) < 1 && i > 0) {
+                hashMapReplaceByKey(hashMapSkippedCurrent, curChar, 1);
                 i--;
                 curChar = s.charAt(i);
             }
-            if (hashMapSkipped.get(curChar) - hashMapCurrentQty.get(curChar) + hashMap.get(curChar) > 0) {
-                hashMapSkipped.replace(curChar, hashMapSkipped.get(curChar) - 1);
+            hashMapReplaceByKey(hashMapSkippedCurrent, curChar, 0);
+
+            //Enter if letter can be skipped
+            if (hashMapSkipped.get(curChar) - hashMapSkippedCurrent.get(curChar) > 0) {
+                hashMapReplaceByKey(hashMapSkippedCurrent, curChar, 1);
                 for (int j = i - 1; j > strLenght - result.length() - 1; j--) {
                     char ch = s.charAt(j);
+                    hashMapReplaceByKey(hashMapSkippedCurrent, ch, 0);
                     if (hashMapCurrentQty.get(ch) > 0) {
                         if (ch < curChar) {
                             curChar = ch;
                             i = j;
-                            if (hashMapSkipped.get(ch) - hashMapCurrentQty.get(ch) + hashMap.get(ch) < 1) {
+                            //can be used or skipped
+                            if (hashMapSkipped.get(ch) - hashMapSkippedCurrent.get(ch) < 1) {
                                 break;
                             }
 
                         } else {
-                            if (hashMapSkipped.get(ch) - hashMapCurrentQty.get(ch) + hashMap.get(ch) < 1) {
+                            if (hashMapSkipped.get(ch) - hashMapSkippedCurrent.get(ch) < 1) {
                                 break;
                             }
                         }
                     }
-                    hashMapSkipped.replace(s.charAt(j), hashMapSkipped.get(s.charAt(j)) - 1);
+                    hashMapReplaceByKey(hashMapSkippedCurrent, s.charAt(j), 1);
                 }
             }
             if (result.length() < strLenght) {
                 result.append(curChar);
-                hashMapCurrentQty.replace(curChar, hashMapCurrentQty.get(curChar) - 1);
+                hashMapReplaceByKey(hashMapCurrentQty, curChar, -1);
+                hashMapReplaceByKey(hashMapSkipped, curChar, 1);
+                for (int j = lastIndex; j >= i; j--) {
+                    hashMapReplaceByKey(hashMapSkipped, s.charAt(j), -1);
+                }
             }
         }
         return result.toString();
+    }
+
+    private static void hashMapReplaceByKey(HashMap<Character, Integer> hashMap, char ch, int delta) {
+        if (hashMap.containsKey(ch)) {
+            hashMap.replace(ch, hashMap.get(ch) + delta);
+        } else {
+            hashMap.put(ch, delta);
+        }
     }
 }
