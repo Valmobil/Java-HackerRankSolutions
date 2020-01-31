c1430dc208920a403adf1318d68ee1e75efa24e9
Valeriy Trapeznikov <valeriy.trapeznikov@entromy.com>
2020-01-31 10:03:24 +0200

    Reflection: Get Methods

--- src/main/java/reflectionTestAnyObject/ReflectionTestAnyObject.java
+++ src/main/java/reflectionTestAnyObject/ReflectionTestAnyObject.java
@@ -1,8 +1,9 @@
 package reflectionTestAnyObject;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.text.MessageFormat;
+import java.lang.reflect.Parameter;
 import javax.swing.JOptionPane;
 
 public class ReflectionTestAnyObject {
@@ -22,27 +23,54 @@ public class ReflectionTestAnyObject {
       if (superClass != null && Object.class != superClass) {
         System.out.println("SuperClass name: " + superClass.getName());
       }
+      System.out.println();
       printConstructors(cl);
+      System.out.println();
+      printMethods(cl);
     } catch (ClassNotFoundException e) {
       e.printStackTrace();
     }
   }
 
+  private static void printMethods(Class cl) {
+    Method[] methods = cl.getDeclaredMethods();
+    System.out.println("List of Methods:");
+    for (int i = 0; i < methods.length ; i++) {
+      Method method = methods[i];
+      Class retType = method.getReturnType();
+      String name = method.getName();
+      String modifier = Modifier.toString(method.getModifiers());
+
+      System.out.format("%s %s %s %s", modifier, retType, name, "(");
+      Parameter[] parameters = method.getParameters();
+      for (int j = 0; j < parameters.length; j++) {
+        if (j > 0) {
+          System.out.print(", ");
+        }
+        String param = parameters[j].toString();
+        System.out.print(param);
+      }
+      System.out.println(");");
+    }
+  }
+
   private static void printConstructors(Class cl) {
     Constructor[] constructors = cl.getConstructors();
+    System.out.println("Constructors:");
     for (int i = 0; i < constructors.length; i++) {
       Constructor constructor = constructors[i];
       String name = constructor.getName();
-      System.out.println(Modifier.toString(cl.getModifiers()));
-      System.out.println("Constructor name: " + name + "{");
+      System.out.print(Modifier.toString(cl.getModifiers()));
+      System.out.print(" " + name + "(");
       Class[] parameterTypes = constructor.getParameterTypes();
       for (int j = 0; j < parameterTypes.length; j++) {
         if (j > 0) {
           System.out.print(", ");
         }
-        System.out.println(parameterTypes[j].getName());
+        System.out.print(parameterTypes[j].getName());
+
       }
-      System.out.println("};");
+      System.out.println(");");
     }
 
   }
